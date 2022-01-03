89dd54eb60d802d8634453504aef04ea53135d6c
austinkloske22 <austinkloske@gmail.com>
2022-01-03 13:44:50 +0100

    connect wallet

---
+++ build/precache-manifest.c8a619e597847c6e8baf0c7b8673148d.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "ad888d301582b4268a8b82363324a32b",
+    "url": "/index.html"
+  },
+  {
+    "revision": "b6be7ef51541cb1d472b",
+    "url": "/static/css/main.0e632e19.chunk.css"
+  },
+  {
+    "revision": "ef589e3f65a96b123a0a",
+    "url": "/static/js/2.307d5f75.chunk.js"
+  },
+  {
+    "revision": "b6be7ef51541cb1d472b",
+    "url": "/static/js/main.cc3856d2.chunk.js"
+  },
+  {
+    "revision": "f67c199efb9f2c29f389",
+    "url": "/static/js/runtime-main.f294effb.js"
+  }
+]);
